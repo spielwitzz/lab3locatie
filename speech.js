fb8693d36862f5a03dcd34564a7ce3e3106d1932
spielwitzz <115624650+spielwitzz@users.noreply.github.com>
2022-10-19 14:44:38 +0300

    Update speech.js

--- speech.js
+++ speech.js
@@ -20,5 +20,5 @@ function onend()
 	recognition.onresult =on_results;
 function on_results(e)
 {
-document.getElementById("text").innerHTML += "Ati rostit cuvantul: "+ e.results[0][0].transcript + ", acuratete: "+e.results[0][0].confidence + "<br>"
+	document.getElementById("text").innerHTML += "Ati rostit cuvantul: "+ e.results[0][0].transcript + ", acuratete: "+e.results[0][0].confidence + "<br>";
 }
