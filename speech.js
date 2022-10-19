9bee51e129b04042ed6553f8dbe0b2175e0b1ebb
spielwitzz <115624650+spielwitzz@users.noreply.github.com>
2022-10-19 15:02:34 +0300

    Update speech.js

--- speech.js
+++ speech.js
@@ -2,6 +2,7 @@ document.addEventListener("touchstart",on_touch);
 document.addEventListener("mousedown",on_touch);
 var recognition = new webkitSpeechRecognition();
 recognition.lang = 'en-US';
+try{
 function on_touch()
 {
 	if(recognition.start)
@@ -10,10 +11,13 @@ function on_touch()
 		recognition_started = true;
 	}
 }
+}
+catch{
 function onend()
 {
 	recognition.stop();
 	recognition_started = false;
+}
 }
 	recognition.onend = onend;
 	recognition.onsoundend = onend;
@@ -21,5 +25,5 @@ function onend()
 	recognition.onresult =on_results;
 function on_results(e)
 {
-	document.getElementById("text").innerHTML += "Ati rostit cuvantul: "+ e.results[0][0].transcript + ", acuratete: "+e.results[0][0].confidence + "<br>";
+document.getElementById("text").innerHTML += "Ati rostit cuvantul: "+ e.results[0][0].transcript + ", acuratete: "+e.results[0][0].confidence + "<br>";
 }
